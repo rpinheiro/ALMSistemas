a01d7158b8d67df67ca2a984c87497bd82557d9d
rpinheiro <rafael.06@gmail.com>
2018-01-21 17:34:51 -0200

    Primeiro commit do projeto

---
+++ Models/Context/ALMContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ALMSistemas.Models;
+
+namespace ALMSistemas.Models.Context
+{
+    public class ALMContext : DbContext
+    {
+        #region|Propriedades|
+        public DbSet<Usuario> Usuarios { get; set; }
+        #endregion|Propriedades|
+
+        #region|Construtor|
+        public ALMContext()
+            : base("DefaultConnection")
+        {
+        }
+        #endregion|Construtor|
+
+    }
+}
---
+++ Scripts/app.js
@@ -0,0 +1,10 @@
+﻿var app = angular.module("ALMSistemasApp", ["ALMSistemasApp.controllers", "ngRoute"]);
+
+app.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider.
+    when("/", { templateUrl: "/Partials/BemVindo.html", controller: "MainController" }).
+    when("/CadastroUsuario", { templateUrl: "/Partials/Usuario/AddUsuario.html", controller: "AddUsuarioController" }).
+    when("/ListarUsuarios", { templateUrl: "/Partials/Usuario/ListUsuario.html", controller: "ListUsuarioController" }).
+    when("/AtualizarUsuario/:id", { templateUrl: "/Partials/Usuario/UpdateUsuario.html", controller: "UpdateUsuarioController" }).    
+    otherwise({ redirectTo: "/" });
+}])
